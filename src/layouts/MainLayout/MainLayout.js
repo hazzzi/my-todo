114a9a8808dd5faf48ec5b79640b16fae9340f91
hazzzi <hanee_l@naver.com>
2019-12-18 11:49:57 +0900

    파일 정리 package.json proptype 추가

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -1,13 +1,13 @@
 import { Box, Typography } from '@material-ui/core';
 import React from 'react';
-import Home from '../../views/Home';
+import TodoList from '../../views/TodoList';
 
 const MainLayout = () => (
   <Box>
     <Box display="flex" justifyContent="center" padding={7} bgcolor="primary.main" color="primary.contrastText" mb={1}>
       <Typography variant="h3">TODO LIST</Typography>
     </Box>
-    <Home />
+    <TodoList />
   </Box>
 );
 
