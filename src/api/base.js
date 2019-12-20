d856db26c99b7e71b38c23daf80ae3c03665cb31
hazzzi <hanee_l@naver.com>
2019-12-20 17:53:20 +0900

    CRUD api 요청 추가

--- src/api/base.js
+++ src/api/base.js
@@ -2,16 +2,68 @@ import axios from 'axios'
 
 export const server = axios.create({ baseURL: 'http://localhost:8080' })
 
+const extractApiData = apiResponse => {
+  return apiResponse ? apiResponse.data : ""
+}
+
 class ApiHelper {
-  postJsonData = (url, requestData) => server.post(url, requestData)
-    .catch((thrown) => {
-      if (axios.isCancel(thrown)) {
-        console.log('request canceled', thrown.message)
-      } else {
-        throw thrown
+  postJsonData = (url, requestData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
       }
-    })
-    .then((apiResponse) => apiResponse)
+    }
+    return server.post(url, requestData, config)
+      .catch((thrown) => {
+        if (axios.isCancel(thrown)) {
+          console.log('request canceled', thrown.message)
+        } else {
+          throw thrown
+        }
+      })
+      .then((apiResponse) => extractApiData(apiResponse))
+  }
+
+  get = (url, requestData) => {
+    return server.get(url, requestData)
+      .catch((thrown) => {
+        if (axios.isCancel(thrown)) {
+          console.log('request canceled', thrown.message)
+        } else {
+          throw thrown
+        }
+      })
+      .then((apiResponse) => extractApiData(apiResponse))
+  }
+
+  putJsonData = (url, requestData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    return server.put(url, requestData, config)
+      .catch((thrown) => {
+        if (axios.isCancel(thrown)) {
+          console.log('request canceled', thrown.message)
+        } else {
+          throw thrown
+        }
+      })
+      .then((apiResponse) => extractApiData(apiResponse))
+  }
+
+  delete = (url, requestData) => {
+    return server.delete(url, requestData)
+      .catch((thrown) => {
+        if (axios.isCancel(thrown)) {
+          console.log('request canceled', thrown.message)
+        } else {
+          throw thrown
+        }
+      })
+      .then((apiResponse) => extractApiData(apiResponse))
+  }
 }
 
 export const helper = new ApiHelper()
--- src/api/todo.js
+++ src/api/todo.js
@@ -1,7 +1,31 @@
 import { helper } from './base'
 
-export default async function createTodo() {
+export const createTodo = async (initData) => {
   const url = '/api/todos'
-  const response = await helper.postJsonData(url)
+  const response = await helper.postJsonData(url, initData)
+  return response
+}
+
+export const todoList = async() => {
+  const url = '/api/todos'
+  const response = await helper.get(url)
+  return response
+}
+
+export const todoInfo = async(todoId) => {
+  const url = `/api/todos/${todoId}`
+  const response = await helper.get(url)
+  return response
+}
+
+export const todoUpdate = async(todoId, data) => {
+  const url = `/api/todos/${todoId}`
+  const response = await helper.putJsonData(url, data)
+  return response
+}
+
+export const todoDelete = async(todoId) => {
+  const url = `/api/todos/${todoId}`
+  const response = await helper.delete(url, data)
   return response
 }
